f93c0a9d95e2d9561e0dd7956c84313368fd11b2
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2025-02-19 17:02:10 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,26 @@ import Blog from "./components/Blog.jsx";
 import Menu from "./components/Menu.jsx";
 import Contend from "./components/Contend.jsx";
 import Pricing from "./components/Pricing.jsx";
-import Footer from "./components/Footer.jsx";
 import Home from './pages/Home.jsx';
 import Login from "../src/components/Login.jsx";
 import Sinup from "./components/Sinup.jsx";
 import Hammasi from "./components/Hammasi.jsx";
+import Layout from "./components/Layout.jsx";
 function App() {
   return (
     <div className="bg-[#F9F9F9]">
-      <Navbar />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/contact" element={<Contend />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/sinup" element={<Sinup/>} />
-          <Route path="/hammasi" element={<Hammasi/>} />
+          <Route path="/" element={<Layout><Home/></Layout>} />
+          <Route path="/menu" element={<Layout><Menu /></Layout>} />
+          <Route path="/blog" element={<Layout><Blog /></Layout>} />
+          <Route path="/pricing" element={ <Layout><Pricing/></Layout>} />
+          <Route path="/contact" element={<Layout><Contend /></Layout>} />
+          <Route path="/login" element={<Login />} />
+           <Route path="/sinup" element={<Sinup/>} />
+          <Route path="/hammasi" element={<Layout><Hammasi/></Layout>} />
         </Routes>
       </main>
-      <Footer/>
     </div>
   );
 }
--- src/components/Sinup.jsx
+++ src/components/Sinup.jsx
@@ -1,9 +1,140 @@
-import React from 'react'
+import * as React from "react";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import g from "../assets/G.png";
+import pa from "../assets/pa.png";
+import { Link } from "react-router-dom";
+import imsga from "../assets/imga.png"
 
 function Sinup() {
   return (
-    <div className='h-[100vh]'>
-         <p>Sign Up To eatly</p>
+    <div className="h-[100vh] flex justify-center items-center bg-white">
+      <div className="h-screen w-[100%] flex justify-center items-center">
+        <div className="p-4">
+          <p className="text-4xl font-[Poppins] font-[600] text-[#323142] text-center p-8">
+          Sign Up To eatly
+          </p>
+          <div className="flex justify-center m-3">
+            <Box sx={{ "& button": { m: 1 } }}>
+              <div>
+                <Button
+                  sx={{
+                    background: "#F5F5F5",
+                    boxShadow: "none",
+                    height: "63px",
+                    width: "185px",
+                    borderRadius: "14px",
+                  }}
+                  variant="contained"
+                  size="large"
+                >
+                  <img src={g} alt="" />
+                </Button>
+                <Button
+                  sx={{
+                    background: "#F5F5F5",
+                    boxShadow: "none",
+                    height: "63px",
+                    width: "185px",
+                    borderRadius: "14px",
+                  }}
+                  variant="contained"
+                  size="large"
+                >
+                  <img src={pa} alt="" />
+                </Button>
+              </div>
+            </Box>
+          </div>
+          <div className="flex justify-center align-center flex-wrap m-4">
+            <TextField
+              variant="outlined"
+              placeholder="Name"
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  border: "none",
+                  borderRadius: "16px",
+                  backgroundColor: "#F5F5F5",
+                  width: "382px",
+                  padding: "8px",
+                  marginTop: "15px",
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+              }}
+            />
+            <TextField
+              variant="outlined"
+              placeholder="Email"
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  border: "none",
+                  borderRadius: "16px",
+                  backgroundColor: "#F5F5F5",
+                  width: "382px",
+                  padding: "8px",
+                  marginTop: "15px",
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+              }}
+            />
+            <TextField
+              variant="outlined"
+              placeholder="Password"
+              sx={{
+                "& .MuiOutlinedInput-root": {
+                  border: "none",
+                  borderRadius: "16px",
+                  backgroundColor: "#F5F5F5",
+                  width: "382px",
+                  padding: "8px",
+                  marginTop: "15px",
+                },
+                "& .MuiOutlinedInput-notchedOutline": {
+                  border: "none",
+                },
+              }}
+            />
+            <Box sx={{ "& button": { m: 7 } }}>
+              <Button onClick={()=> window.location.pathname = '/'}
+                sx={{
+                  width: "382px",
+                  padding: "17px",
+                  borderRadius: "15px",
+                  background: "#6C5FBC",
+                }}
+                variant="contained"
+                size="large"
+              >
+                Sign in
+              </Button>
+            </Box>
+            <p className="text-[18px] font-[Poppins] text-[#3E3E3E]">
+              Create A New Account?{" "}
+              <Link className="text-[#6C5FBC]" to={"/login"}>
+              Sign In
+              </Link>{" "}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="h-[100vh] w-[100%] bg-[#6C5FBC] flex align-center justify-center flex-wrap flex-col gap-5">
+        <div className="flex justify-center p-0">
+          <img className="h-[400px]" src={imsga} alt="" />
+        </div>
+        <div>
+          <p className="text-[40px] font-extrabold text-white font-[Manrope] text-center m-2">Find Foods With Love </p>
+          <p className="text-[17px] text-center font-[Manrope] text-white px-18">
+            Eatly Is The Food Delivery Dashboard And Having More Than 2K+ Dishes
+            Including Asian, Chinese, Italians And Many More. Our Dashboard
+            Helps You To Manage Orders And Money.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
